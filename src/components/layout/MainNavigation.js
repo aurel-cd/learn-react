f25e5bebf314cc76ae449653b1831bd92da769bc
aurel-cd <aurelspahiu0@gmail.com>
2023-11-12 13:06:11 +0100

    basics done

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,25 +1,33 @@
-import { Link } from 'react-router-dom';
-import classes from './MainNavigation.module.css';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
+import FavouritesContext from "../../store/favourites-context";
 
 function MainNavigation() {
-    return (
-      <header className={classes.header}>
-        <div className={classes.logo}>React Meetups</div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>All Meetups</Link>
-            </li>
-            <li>
-              <Link to='/new-meetup'>New Meetup</Link>
-            </li>
-            <li>
-              <Link to='/favourites'>Favourites</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    )
+  const favouritesCtx = useContext(FavouritesContext);
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">New Meetup</Link>
+          </li>
+          <li>
+            <Link to="/favourites">
+              Favourites
+              <span className={classes.badge}>
+                {favouritesCtx.totalFavourites}
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default MainNavigation;
